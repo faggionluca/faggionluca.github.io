43d2550ac5b7401df5a7e889cefed6d896ace349
Darkimage <luc-af@live.it>
2020-08-18 15:47:49 +0200

    Initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrbadgeComponent } from './crbadge/crbadge.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProjectContainerComponent } from './project-container/project-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrbadgeComponent,
+    SidebarComponent,
+    ProjectContainerComponent
   ],
   imports: [
+    FlexLayoutModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
