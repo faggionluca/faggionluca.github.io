501905b5084bc23ec170c060da15c926226032af
Darkimage <luc-af@live.it>
2020-08-19 00:31:40 +0200

    Dynamic templating for crbadge

--- src/app/crbadge/crbadge.component.ts
+++ src/app/crbadge/crbadge.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'app-crbadge',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrbadgeComponent implements OnInit {
 
+  @Input() personalDetails: Map<string, string>;
+
+  originalOrder = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
+    return 0;
+  }
   constructor() { }
 
   ngOnInit() {
