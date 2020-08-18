1c3fc2821a12b615c84c096596628c91a9f3fc4c
Darkimage <luc-af@live.it>
2020-08-19 00:31:56 +0200

    Added initial test data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'LFcurriculum';
+  title = 'Resume';
   sideItems: Map<string, string> = new Map([
-    [ ':classical_building: Universita\'', 'Universita degli studi di Parma.']
+    [':classical_building: Universita\'',
+      'Universita degli studi di Parma.'],
+
+    [':keyboard: Principali linguaggi',
+      'C++, Python, Typescript, Java'],
+
+    [':mortar_board: Corso',
+      'Laurea in Ingegneria Informatica, Elettronica e delle Telecomunicazioni (LIET)']
+  ]);
+  personalDetails: Map<string, string> = new Map([
+    ['Nome:', 'Luca'],
+    ['Cognome:', 'Faggion'],
+    ['Data di Nascita:', '04/12/1995']
   ]);
 }
