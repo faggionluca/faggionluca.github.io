c235bf1e0220c40b53adb2c4d61ea7e640fc2438
Darkimage <luc-af@live.it>
2020-08-20 19:06:50 +0200

    Project component now support input

--- src/app/project-container/project-container.component.ts
+++ src/app/project-container/project-container.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { project } from '../app.component';
 
 @Component({
   selector: 'app-project-container',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-container.component.scss']
 })
 export class ProjectContainerComponent implements OnInit {
+
+  @Input() project: project;
 
   constructor() { }
 
