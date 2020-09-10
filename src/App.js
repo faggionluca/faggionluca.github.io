46420dfb5ae11f0e2904aa6aab719aac7d9e77fc
Darkimage <luc-af@live.it>
2020-09-11 00:14:39 +0200

    Testing printing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Prova(props) {
+  return (
+    <a href={props.link}>{props.test}</a>
+  );
+}
+
 function App() {
+  var test = [];
+  for (let i = 0; i < 25; i++) {
+    test.push(<p>prova - {i}</p>)
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Prova</h1>
+        <Prova test="ciao" link="https://github.com/KyleAMathews/typography.js"></Prova>
+        <hr />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +32,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {test}
     </div>
   );
 }
