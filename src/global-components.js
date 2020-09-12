3c854526db9807fca2a96196fddcf24d1cf2990e
Darkimage <luc-af@live.it>
2020-09-12 19:58:51 +0200

    Added features to withAnimated and withAnimatedChildren  - now support keeping css classes and inject style  - added withAnimatedChildren to animate a group of withAnimated components

--- src/global-components.js
+++ src/global-components.js
@@ -1,28 +1,68 @@
 import styled from 'styled-components';
 import React from 'react';
 
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
 export function withAnimated(StyledComponent, animateClass) {
-  const classes = [];
-  for (const key in animateClass) {
-    if (animateClass.hasOwnProperty(key)) {
-      const value = animateClass[key];
-      if (key !== 'animation') {
-        classes.push(`animate__${key}-${value}`)
-      } else {
-        classes.push(`animate__${value}`)
-      }
-    }
-  }
-	return (props) => {
-		const { className, ...rest } = props;
-    return (<StyledComponent {...rest} className={`${className} ${ (classes.length !== 0) ? "animate__animated" : ""} ${classes.join(" ")}`}/>)
-  };
+	const withAnimated = (props) => {
+		const classes = [];
+		const cssKeyClasses = ['animation', 'delay', 'speed'];
+		animateClass = {...animateClass, ...props.animateCss}
+		for (const key of cssKeyClasses) {
+			if (animateClass.hasOwnProperty(key)) {
+				const value = animateClass[key];
+				if (key !== 'animation') {
+					classes.push(`animate__${key}-${value}`)
+				} else {
+					classes.push(`animate__${value}`)
+				}
+			}
+		}
+		const { className, style, ...rest } = props;
+		const { animation, delay, speed, ...animateStyle } = animateClass;
+		const styleElem = { ...style, ...animateStyle };
+		return (<StyledComponent {...rest} style={styleElem} className={`${className} ${ (classes.length !== 0) ? "animate__animated" : ""} ${classes.join(" ")}`}/>)
+	};
+	withAnimated.displayName = `WithAnimated(${getDisplayName(StyledComponent)})`;
+	return withAnimated;
+}
+
+export function withAnimatedChildren(StyledComponent, animateOptions) {
+	const computeTime = (offset, damping, startOffset) => {
+		const sec = offset*damping + (startOffset ? startOffset : 0);
+		return sec ? `${sec}s` : undefined;
+	}
+
+	const constructAnimCss = (offset) => {
+		const css = {
+			animationDelay: computeTime(offset, animateOptions.dampingDelay, animateOptions.delay),
+			animationDuration: computeTime(offset, animateOptions.dampingDuration, animateOptions.startOffset)
+		};
+		Object.keys(css).forEach(key => css[key] === undefined && delete css[key])
+		return css
+	}
+	const withAnimatedChildren = (props) => {
+		const children = React.Children.map(props.children, (Child, i) => {
+			return React.cloneElement(Child, {
+				animateCss: constructAnimCss(i)
+				})
+			}
+		);
+		return <StyledComponent {...props}>{children}</StyledComponent>
+	};
+	withAnimatedChildren.displayName = `withAnimatedChildren(${getDisplayName(StyledComponent)})`;
+	return withAnimatedChildren;
 }
 
 export const theme = {
-	primary: '#dedede'
+	primary: '#f5f5f5',
+	boxShadows: `0 4.8px 6.3px rgba(0, 0, 0, 0.12), 0 38px 50px rgba(0, 0, 0, 0.14)`,
+	borderColor: '#999999'
 }
 
 export const Hr = styled.hr`
-	width: 100%
+	width: 100%;
+	background: hsla(0,0%,0%,0.1);
 `;
