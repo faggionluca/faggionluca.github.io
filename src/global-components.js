8df57976c0ebf148dccf14860309dc0d84dac4c7
Darkimage <luc-af@live.it>
2020-10-03 13:06:49 +0200

    Added breakpoints object

--- src/global-components.js
+++ src/global-components.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const breakpoints = {
+	sm: "576px",
+	md: "768px",
+	lg: "920px",
+	xl: "1200px"
+}
+
 export const theme = {
 	primary: '#f5f5f5',
 	boxShadows: `0 4.8px 6.3px rgba(0, 0, 0, 0.12), 0 38px 50px rgba(0, 0, 0, 0.14)`,
