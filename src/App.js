8a516388dfa9d373f787553b9199ca170d960113
Darkimage <luc-af@live.it>
2020-10-31 12:43:25 +0100

    Fixed layout centering Initial data rendering

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import photo from './assets/images/photo.jpg';
 import data from './assets/data.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// https://stackoverflow.com/a/21984136/6791579
+function _calculateAge(birthday) { // birthday is a date
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
 const AppContainerWrapper = styled.div`
   display: grid;
 	width: 100%;
@@ -43,23 +50,20 @@ const AppContainer = styled(Container)`
 `
 
 const TopSideContainer = function(props) {
-  return <Col xs="12" className={`d-flex pt-3 justify-content-center ${props.className}`}>{props.children}</Col>
+  return <Col xs="12" className={`d-flex pt-3 justify-content-center align-items-center ${props.className}`}>{props.children}</Col>
 }
 
 const Avatar = function () {
-  const AvatarImg = styled.div`
-    min-width: 256px;
-  `
   const AvatarContainer = styled(Col)`
     @media only screen and (max-width: ${breakpoints.md}){
       min-height: 10rem;
     }
   `
-  
-  return <AvatarContainer className="justify-content-center d-flex order-1 order-sm-1 order-lg-2" md="12" sm="12" lg="auto">
-    <AvatarImg className="card-profile-image">
+
+  return <AvatarContainer className="justify-content-center d-flex order-1 order-sm-1 order-lg-2 p-0" md="12" sm="12" lg="auto">
+    <div className="card-profile-image">
       <img className="m-0 rounded-circle" src={photo}></img>
-    </AvatarImg>
+    </div>
   </AvatarContainer>
 }
 
@@ -78,31 +82,44 @@ const SocialIcon = function (props) {
   return <div {...props} className="pl-4">
     <LinkIcon href={props.icon.link}>
       <FontAwesomeIcon size="3x" icon={props.icon.icon} />
-      <p>{props.icon.name}</p>
+      <span>{props.icon.name}</span>
     </LinkIcon>
   </div>
 }
 
 const SocialIcons = function (props) {
   const icons = data.social.map((icon, index) => <SocialIcon key={index} icon={icon}/>)
-
-  return <Container className="justify-content-center">
-    <Row className="justify-content-center">
-      {icons}
-    </Row>
-  </Container>
+  return <Row className="justify-content-center" fluid>{icons}</Row>
 }
 
 const TopBar = function() {
-  return <Container fluid>
-    <Row className="justify-content-center">
-      <TopSideContainer className="order-2 order-sm-2 col-sm-6 order-lg-1 col-lg-3">
+  return <Row className="justify-content-center" fluid>
+      <TopSideContainer className="order-2 order-sm-2 col-sm-6 order-lg-1 col-lg-6">
         <SocialIcons/>
       </TopSideContainer>
       <Avatar/>
-      <TopSideContainer className="order-3 order-md-3 col-sm-6 order-lg-3 col-lg-3"/>
+      <TopSideContainer className="order-3 order-md-3 col-sm-6 order-lg-3 col-lg-6" >
+      <a href={`mailto:${data.contact_email}`}><Button color="primary">Contact Me</Button></a>
+      </TopSideContainer>
     </Row>
-  </Container>
+}
+
+const PersonalDetails = function () {
+  const DetailsContainer = styled(Col)`min-width: 256px;`;
+
+  return <Row fluid className="justify-content-center d-flex mt-5">
+    <DetailsContainer lg='auto' className="justify-content-center text-center">
+      <h3>
+        {`${data.details.name} ${data.details.surname}`}
+        <span className="font-weight-light">
+          {`, ${_calculateAge(new Date(data.details.birthdate))}`}
+        </span>
+      </h3>
+      <div className="h6 font-weight-300">
+        {data.details.city}, {data.details.state}
+      </div>
+    </DetailsContainer>
+  </Row>
 }
 
 function App() {
@@ -110,12 +127,8 @@ function App() {
     <>
     <AppContainerWrapper className="profile-page">
       <AppContainer className="card card-profile shadow mt-0" fluid>
-        <TopBar/>
-        <Container fluid>
-          <Row className="w-100">
-            <Col className="h-100">prova</Col>
-          </Row>
-        </Container>
+        <TopBar />
+        <PersonalDetails/>
       </AppContainer>
       </AppContainerWrapper>
       <BgStatic/>
