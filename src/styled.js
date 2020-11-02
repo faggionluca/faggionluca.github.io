b1bc08dd761bbb4749ae4cc50ff5c79a2a8b9bcc
Darkimage <luc-af@live.it>
2020-11-02 15:59:36 +0100

    Refactor moved styled components to separated file

---
+++ src/styled.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+import { Container, Col, Row } from 'reactstrap';
+import { breakpoints } from './global-components';
+import bg from './assets/images/bg.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const AppWrapper = styled.div`
+	display: grid;
+	width: 100%;
+	height: 100%;
+	padding: 20vh 20vw;
+
+	@media only screen and (max-width: ${breakpoints.md}){
+		padding: 10vh 15vw;
+	}
+
+	@media only screen and (max-width: ${breakpoints.xl}){
+		padding: 15vh 10vw;
+	}
+
+	@media print {
+		padding: 15vh 5vw;
+	}
+`;
+
+export const BgStatic = styled.div`
+	position: absolute;
+	z-index: -1;
+	top: 0;
+	bottom: 0;
+	width: 100%;
+	height: 100%;
+	background-image: url(${bg});
+	background-size: cover;
+	background-position: center center;
+	clip-path: polygon(0 0, 100% 0, 100% 40%, 0 48%);
+`
+
+export const AppContainer = styled(Container)`
+	background-color: #fff;
+`
+
+export const AvatarContainer = styled(Col)`
+	@media only screen and (max-width: ${breakpoints.md}){
+		min-height: 10rem;
+	}
+`
+
+export const IconContainer = styled.div`
+	display: flex;
+	flex-flow: column;
+	justify-content: center;
+	align-items: center;
+	p {
+		font-size: 0.8rem;
+	}
+`
+
+export const MoreContainer = styled(Row)`
+	overflow: hidden;
+	box-sizing: border-box;
+	padding-top: 0;
+	max-height: 0;
+	height: 100%;
+	width: 100%;
+	transition: all 0.3s ease-out;
+`
+
+export const MoreChevron = styled(FontAwesomeIcon)`
+	position: absolute;
+	background-color: #fff;
+`
