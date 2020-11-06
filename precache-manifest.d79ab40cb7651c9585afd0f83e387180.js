29704d7de7f94443cefc09dc6c5ce731e9631d1c
DarkImage <luc-af@live.it>
2020-11-06 22:22:34 +0100

    Updates

---
+++ precache-manifest.d79ab40cb7651c9585afd0f83e387180.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81472de3faf574ff3597d66c60a8ab90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91da0385b8241cdf6eb",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "fd09aa7bb3adb7998c5f",
+    "url": "/static/css/main.735a884e.chunk.css"
+  },
+  {
+    "revision": "f91da0385b8241cdf6eb",
+    "url": "/static/js/2.84f52def.chunk.js"
+  },
+  {
+    "revision": "78de36597c4ced3591bdd707cbe1d93e",
+    "url": "/static/js/2.84f52def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd09aa7bb3adb7998c5f",
+    "url": "/static/js/main.a8184d6e.chunk.js"
+  },
+  {
+    "revision": "939053bcbb85788739d3",
+    "url": "/static/js/runtime-main.a5306fb7.js"
+  },
+  {
+    "revision": "189f1ac0b028062f0b923e9c219dd777",
+    "url": "/static/media/basididati.189f1ac0.png"
+  },
+  {
+    "revision": "50ebc7be9c6d72c352d602a8d055c6fe",
+    "url": "/static/media/bg.50ebc7be.svg"
+  },
+  {
+    "revision": "2a07b65d022ba3781705fba0f8aa63d5",
+    "url": "/static/media/ingegneriadelsoftware.2a07b65d.png"
+  },
+  {
+    "revision": "2b3d7279af0392181b76885b568e5a75",
+    "url": "/static/media/mobdevandroid.2b3d7279.jpg"
+  },
+  {
+    "revision": "f7ff075258e738e48ef5c3c8372546f9",
+    "url": "/static/media/mobdevios.f7ff0752.png"
+  },
+  {
+    "revision": "2dc695833f13eb26db71070993908dd2",
+    "url": "/static/media/photo.2dc69583.jpg"
+  }
+]);
