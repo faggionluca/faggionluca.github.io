e7d19a9af1ae72fddf23861b39491ec0e27850e9
DarkImage <luc-af@live.it>
2023-02-13 15:24:22 +0000

    Working Docker + Vite HMR Added react-bootstrap Working Sass

--- luca-resume/vite.config.ts
+++ luca-resume/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // for Docker + HMR -> https://github.com/Subwaytime/vite-aliases/issues/33#issuecomment-1424061997
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5000,
+    watch: {
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
 })
